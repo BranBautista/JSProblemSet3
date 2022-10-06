051a903ba25a0bc395fe51e6e75bebd4fdfcfe63
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-06 18:07:04 -0500

    Update main.js

--- exercise1/main.js
+++ exercise1/main.js
@@ -2,13 +2,14 @@ const submit = document.getElementById("btnsubmit");
 const titleTag = document.getElementById("notetitleinput");
 const bodyTag = document.getElementById("notebodyarea");
 const notesArea = document.getElementById("notes");
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//We save in 'notes' all the notes in the local storage
+const notesLocalStorage = localStorage.getItem("notes");
+//we pass the string into an object with JSONparse, if there no data we pass an empty array
+const notes = JSON.parse(notesLocalStorage || "[]");
 
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-//Get all the notes in the local storage if there exist and parse them to an object. Else passing and empty array
-const notes = JSON.parse(localStorage.getItem("notes") || "[]"); 
-
-function showNotes (){
+//This function will show all the notes on the page
+function showNotes() {
     document.querySelectorAll(".note").forEach(note => note.remove());
     notes.forEach((note, index) => {
         let displayTag = `
@@ -25,46 +26,54 @@ function showNotes (){
             </div>
         </article>
         `;
-        notesArea.insertAdjacentHTML("beforeend",displayTag);
+        notesArea.insertAdjacentHTML("beforeend", displayTag);
     });
 }
 
 showNotes();
 
-function deleteNote(noteId){
-    notes.splice(noteId, 1); //remove selected note from array
+//This function will delete the selected note
+function deleteNote(noteId) {
+    //remove selected note from the array 'notes'
+    notes.splice(noteId, 1);
+    //We pass to a string the 'notes' array
+    notesString = JSON.stringify(notes);
     //save updated notes to local storage
+    localStorage.setItem("notes", notesString);
+    //Show to the page the notes without the deleted one
+    showNotes();
+}
+
+//This function will edit the selected note
+function editNote(noteId, title, body) {
+
+    //Here we put the title and body of the note about to edit, on the input and textarea in order to edit
+    notes.findIndex((element, noteId) => {
+        titleTag.value = element.title;
+        bodyTag.value = element.body;
+    })
+
+    notes.splice(noteId, 1);
     notesString = JSON.stringify(notes);
     localStorage.setItem("notes", notesString);
     showNotes();
 }
 
-function editNote(noteId, title, body) {
-    
-    notes.findIndex((element,noteId) => {
-        titleTag.value = element.title;
-        bodyTag.value = element.body;
-    })
-
-    notes.splice(noteId,1);
-    notesString = JSON.stringify(notes);
-    localStorage.setItem("notes",notesString);
-    showNotes();
-}
-
-submit.addEventListener("click",e => {
+//This event will be to control the submit button
+submit.addEventListener("click", e => {
     e.preventDefault();
     let noteTitle = titleTag.value;
     let noteBody = bodyTag.value;
 
-    if(noteTitle || noteBody) {
+    if (noteTitle || noteBody) {
 
-        //We need to get the date
+        //Here we get the current date
         let dateObj = new Date(),
-        month = months[dateObj.getMonth()],
-        day = dateObj.getDate(),
-        year = dateObj.getFullYear();
+            month = months[dateObj.getMonth()],
+            day = dateObj.getDate(),
+            year = dateObj.getFullYear();
 
+        //We define the structure of the note
         let noteStructure = {
             title: noteTitle,
             body: noteBody,
@@ -72,14 +81,33 @@ submit.addEventListener("click",e => {
         }
 
         // We will save all the notes on the next array
-        notes.push(noteStructure); //Here we are adding the notes to the array notes
-        notesString = JSON.stringify(notes); //We need to convert the array into string, then save in the local.storage
-        localStorage.setItem("notes", notesString); //Here we are saving the notes' array in the local storage
+        // and we will add the notes to the array notes
+        notes.push(noteStructure);
+        notesString = JSON.stringify(notes);
+        localStorage.setItem("notes", notesString);
 
         showNotes();
 
+        //We reset the value of the input and textarea
         titleTag.value = "";
         bodyTag.value = "";
     }
 })
 
+//This event will enable the use of the Tab key
+bodyTag.addEventListener('keydown', (event) => {
+    let start, end, beforeTab, afterTab, newString;
+    if (event.keyCode === 9) {
+        event.preventDefault();
+        start = bodyTag.selectionStart;
+        end = bodyTag.selectionEnd;
+        console.log(start)
+        beforeTab = bodyTag.value.substring(0, start);
+        afterTab = bodyTag.value.substring(end);
+        newString = beforeTab + "\t" + afterTab;
+        bodyTag.value = newString
+        bodyTagselectionStart = start + 1
+        bodyTagselectionEnd = start + 1
+        return false;
+    }
+});
