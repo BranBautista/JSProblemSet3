c32131ca67fbc0149c2de2f683ee9c7783a97b5c
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-17 17:27:00 -0500

    Update main.js

--- exercise1/main.js
+++ exercise1/main.js
@@ -2,31 +2,57 @@ const submit = document.getElementById("btnsubmit");
 const titleTag = document.getElementById("notetitleinput");
 const bodyTag = document.getElementById("notebodyarea");
 const notesArea = document.getElementById("notes");
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let dateHelp = "";
+
+let template = document.getElementById("template")
+
 //We save in 'notes' all the notes in the local storage
 const notesLocalStorage = localStorage.getItem("notes");
 //we pass the string into an object with JSONparse, if there no data we pass an empty array
 const notes = JSON.parse(notesLocalStorage || "[]");
 
+let editRegisterObj = [];
+
+
+
 //This function will show all the notes on the page
 function showNotes() {
     document.querySelectorAll(".note").forEach(note => note.remove());
+
     notes.forEach((note, index) => {
+
+        editRegisterObj.push({
+            title: note.title,
+            body: note.body,
+            id: index,
+            lastEdit: []
+        })
+    
+        editRegisterObj[index].lastEdit.push(note.date);
+
         let displayTag = `
         <article class="note">
-            <div class="details">
+        <div class="details">
                 <p>${note.title}</p>
                 <span>${note.body}</span>
             </div>
             <div class="foot">
                 <span>${note.date}</span>
                 <br>
+                <span>Last edit: ${note.editDate}</span>
+                <br>
                 <a onclick="deleteNote(${index})" class="delete" href="#">Delete</a>
                 <a onclick="editNote(${index}, '${note.title}','${note.body}')" class="edit" href="#">Edit</a>
             </div>
         </article>
         `;
-        notesArea.insertAdjacentHTML("beforeend", displayTag);
+
+        template.innerHTML = displayTag;
+
+        let cloneTemplate = template.content.cloneNode(true);
+
+        notesArea.appendChild(cloneTemplate);
+
     });
 }
 
@@ -34,14 +60,16 @@ showNotes();
 
 //This function will delete the selected note
 function deleteNote(noteId) {
-    //remove selected note from the array 'notes'
-    notes.splice(noteId, 1);
-    //We pass to a string the 'notes' array
-    notesString = JSON.stringify(notes);
-    //save updated notes to local storage
-    localStorage.setItem("notes", notesString);
-    //Show to the page the notes without the deleted one
-    showNotes();
+    if (confirm("You really want to delete the note?")==true) {
+        //remove selected note from the array 'notes'
+        notes.splice(noteId, 1);
+        //We pass to a string the 'notes' array
+        notesString = JSON.stringify(notes);
+        //save updated notes to local storage
+        localStorage.setItem("notes", notesString);
+        //Show to the page the notes without the deleted one
+        showNotes();
+    }
 }
 
 //This function will edit the selected note
@@ -52,10 +80,15 @@ function editNote(noteId, title, body) {
         titleTag.value = element.title;
         bodyTag.value = element.body;
     })
+    
+    let lengthArray = editRegisterObj[noteId].lastEdit.length
+    editRegister = JSON.stringify(editRegisterObj[noteId].lastEdit[lengthArray-1])
+
+    dateHelp = editRegister;
 
     notes.splice(noteId, 1);
     notesString = JSON.stringify(notes);
-    localStorage.setItem("notes", notesString);
+
     showNotes();
 }
 
@@ -68,16 +101,30 @@ submit.addEventListener("click", e => {
     if (noteTitle || noteBody) {
 
         //Here we get the current date
-        let dateObj = new Date(),
-            month = months[dateObj.getMonth()],
-            day = dateObj.getDate(),
-            year = dateObj.getFullYear();
+        let dateObj = new Date()
 
+        let options = {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'America/Los_Angeles'};
+        let currentDate = new Intl.DateTimeFormat('en-US', options).format(dateObj);
+
+        let noteStructure ={};
+        
         //We define the structure of the note
-        let noteStructure = {
-            title: noteTitle,
-            body: noteBody,
-            date: `${month} ${day}, ${year}`
+
+        if (dateHelp == ""){
+            noteStructure = {
+                title: noteTitle,
+                body: noteBody,
+                date: currentDate,
+                editDate: currentDate
+            }
+        }
+        else {
+            noteStructure = {
+                title: noteTitle,
+                body: noteBody,
+                date: currentDate,
+                editDate: dateHelp
+            }
         }
 
         // We will save all the notes on the next array
@@ -101,7 +148,6 @@ bodyTag.addEventListener('keydown', (event) => {
         event.preventDefault();
         start = bodyTag.selectionStart;
         end = bodyTag.selectionEnd;
-        console.log(start)
         beforeTab = bodyTag.value.substring(0, start);
         afterTab = bodyTag.value.substring(end);
         newString = beforeTab + "\t" + afterTab;
